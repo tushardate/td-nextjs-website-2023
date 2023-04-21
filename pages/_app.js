5b9073aa368d4b56ae4df6394c6502d57f1cbec2
Tushar Date <tushardate@gmail.com>
2023-04-21 11:22:54 -0700

    Disabled animation on launch

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,7 @@ export default function App({ Component, pageProps, router }) {
 	return (
 		<AnimatePresence
 			mode="wait"
+			initial={false}
 			onExitComplete={() => window.scrollTo(0, 0)}
 		>
 			<Component {...pageProps} key={router.asPath} />
