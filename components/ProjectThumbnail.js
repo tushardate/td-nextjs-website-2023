6e32e5b0744ec7150ed3dd363b2913ebf63a53ac
Tushar Date <tushardate@gmail.com>
2023-05-24 10:10:37 -0700

    New experimental branch. Fixed project thumbnail.

--- components/ProjectThumbnail.js
+++ components/ProjectThumbnail.js
@@ -1,5 +1,9 @@
 import Link from "next/link";
-import { motion, useAnimationControls } from "framer-motion";
+import {
+	motion,
+	useAnimationControls,
+	useAnimate,
+} from "framer-motion";
 import { useCursorStore } from "./GlobalStore";
 import { useEffect, useLayoutEffect } from "react";
 
@@ -7,9 +11,16 @@ function ProjectThumbnail(props) {
 	const { title, id, slug, project } = props.data;
 	const { cursorType, setCursorType } = useCursorStore();
 	const controls = useAnimationControls();
+	const [scope, animate] = useAnimate();
 
 	useEffect(() => {
-		controls.set("initial");
+		animate(".thumbnailOverlay", { opacity: 0 }, { duration: 0.00001 });
+		animate(
+			".thumbnailInfo p",
+			{ x: -10, opacity: 0 },
+			{ duration: 0.00001 }
+		);
+		animate(".thumbnailImage", { scale: 1 }, { duration: 0.00001 });
 	}, []);
 
 	const transition = {
@@ -17,56 +28,32 @@ function ProjectThumbnail(props) {
 		ease: [0.33, 1, 0.68, 1],
 	};
 
-	const titleAnim = {
-		initial: {
-			y: 5,
-			opacity: 0,
-			transition: transition,
-		},
-		hover: {
-			y: 0,
-			opacity: 1,
-			transition: transition,
-		},
-	};
+	function handleHoverStart() {
+		setCursorType("arrowBottomRight");
+		animate(".thumbnailOverlay", { opacity: 0.72 }, transition);
+		animate(".thumbnailInfo p", { x: 0, opacity: 1 }, transition);
+		animate(".thumbnailImage", { scale: 1.035 }, transition);
+	}
 
-	const overlayAnim = {
-		initial: {
-			opacity: 0,
-			transition: transition,
-		},
-		hover: {
-			opacity: 0.8,
-			transition: transition,
-		},
-	};
-
-	const imageAnim = {
-		initial: {
-			scale: 1,
-			transition: transition,
-		},
-		hover: {
-			scale: 1.035,
-			transition: transition,
-		},
-	};
+	function handleHoverEnd() {
+		setCursorType("default");
+		animate(".thumbnailOverlay", { opacity: 0 }, transition);
+		animate(".thumbnailInfo p", { x: -10, opacity: 0 }, transition);
+		animate(".thumbnailImage", { scale: 1 }, transition);
+	}
 
 	return (
 		<Link as={`/projects/${slug}`} href="/projects/[slug]" scroll={false}>
 			<motion.div
-				onHoverStart={() => setCursorType("arrowBottomRight")}
-				onHoverEnd={() => setCursorType("default")}
+				onHoverStart={() => handleHoverStart()}
+				onHoverEnd={() => handleHoverEnd()}
 				className="project-thumbnail"
+				ref={scope}
 			>
 				<div className="thumbnailRatio relative w-full rounded-lg overflow-hidden safari-fix">
 					<div className="thumbnail absolute w-full h-full">
 						<motion.img
-							variants={imageAnim}
-							animate={controls}
-							onHoverStart={() => controls.start("hover")}
-							onHoverEnd={() => controls.start("initial")}
-							className="object-cover w-full"
+							className="object-cover w-full thumbnailImage"
 							src={`${project.thumbnailImage}tr=w-1024`}
 							srcSet={`${project.thumbnailImage}tr=w-1024 1024w, ${project.thumbnailImage}tr=w-1920 1920w,`}
 							sizes="(min-width: 1024px) 50vw, 100vw"
@@ -74,19 +61,9 @@ function ProjectThumbnail(props) {
 						/>
 					</div>
 					<div className="w-full h-full">
+						<motion.div className="absolute top-0 left-0 w-full h-full bg thumbnailOverlay" />
 						<motion.div
-							variants={overlayAnim}
-							animate={controls}
-							onHoverStart={() => controls.start("hover")}
-							onHoverEnd={() => controls.start("initial")}
-							className="absolute top-0 left-0 w-full h-full bg"
-						/>
-						<motion.div
-							variants={titleAnim}
-							animate={controls}
-							onHoverStart={() => controls.start("hover")}
-							onHoverEnd={() => controls.start("initial")}
-							className={`absolute top-0 left-0 w-full h-full p-9 sm:p-12 lg:p-16 flex flex-col gap-0 lg:gap-0.5 justify-end items-start`}
+							className={`thumbnailInfo absolute top-0 left-0 w-full h-full p-9 sm:p-12 lg:p-16 flex flex-col gap-0 lg:gap-0.5 justify-end items-start`}
 						>
 							<p className="text-white text-base">
 								{project.client}
