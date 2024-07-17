a29de36648673ad28725891b743b1e5f9c60af0e
tushardate <tushardate@gmail.com>
2024-07-17 14:40:00 -0700

    Added lenis scroll that works well with framermotion

--- components/Layout.js
+++ components/Layout.js
@@ -1,18 +1,43 @@
+import Lenis from "lenis";
+import { useRef, useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-// import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
 
-export default function Layout({ children }) {
-	// const lenis = useLenis(({ scroll }) => {
-	// 	// called every scroll
-	// });
+const Layout = ({ children }) => {
+	const lenisRef = useRef(null);
+	const rafHandleRef = useRef(null);
+
+	useEffect(() => {
+		// Initialize Lenis on the first render
+		if (!lenisRef.current) {
+			lenisRef.current = new Lenis();
+			const raf = (time) => {
+				lenisRef.current?.raf(time);
+				rafHandleRef.current = requestAnimationFrame(raf);
+			};
+			rafHandleRef.current = requestAnimationFrame(raf);
+		}
+
+		// Clean up on component unmount
+		return () => {
+			if (lenisRef.current) {
+				lenisRef.current.destroy();
+				lenisRef.current = undefined;
+			}
+			if (rafHandleRef.current) {
+				cancelAnimationFrame(rafHandleRef.current);
+				rafHandleRef.current = null;
+			}
+		};
+	}, []);
+
 	return (
 		<>
-			{/* <ReactLenis root options={{ lerp: 0.06 }}> */}
-				<Header />
-				<div>{children}</div>
-				<Footer />
-			{/* </ReactLenis> */}
+			<Header />
+			<div>{children}</div>
+			<Footer />
 		</>
 	);
-}
+};
+
+export default Layout;
